aadf2155fc8981cfc68744f5649e1d4614e4da4f
alfarizki19 <alfarizki.19.ar@gmail.com>
2025-08-27 22:04:20 +0700

    v1.002.001
    update detail
    - add product detail pop up
    - add function, show pop up product detail
    - add summary pop up

---
+++ assets/js/uiModalProductDetailController/modalProductDetailTypeA.js
@@ -0,0 +1,241 @@
+const modalProductDetail = document.getElementById("productDetailContainerTypeA");
+
+function showModal_ProductDetail() {
+    modalProductDetail.classList.add ("open");
+}
+
+function closeModal_ProductDetail() {
+    modalProductDetail.classList.remove ("open");
+}
+
+document.getElementById('buttonClose_ModalProductDetail').addEventListener('click', function() {
+    closeModal_ProductDetail();
+});
+
+
+/* ----------- Upper Part ----------- */
+
+//Barel
+document.getElementById('buttonProductDetail_barel001001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+document.getElementById('buttonProductDetail_barel002001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+
+//Bolt Carrier Group
+document.getElementById('buttonProductDetail_boltCarrierGroup00100101').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+document.getElementById('buttonProductDetail_boltCarrierGroup00200101').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+document.getElementById('buttonProductDetail_boltCarrierGroup00200201').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+//Charging Handle
+document.getElementById('buttonProductDetail_chargingHandle001001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+document.getElementById('buttonProductDetail_chargingHandle002001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+document.getElementById('buttonProductDetail_chargingHandle003001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+document.getElementById('buttonProductDetail_chargingHandle004001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+//ejection port cover
+document.getElementById('buttonProductDetail_ejectionPortCover001001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+//forward assits
+document.getElementById('buttonProductDetail_forwardAssists001001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+//handguard rail system
+document.getElementById('buttonProductDetail_handguardRailSystem001001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+document.getElementById('buttonProductDetail_handguardRailSystem002001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+//muzzle device
+document.getElementById('buttonProductDetail_muzzleDevice001001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+document.getElementById('buttonProductDetail_muzzleDevice001002').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+document.getElementById('buttonProductDetail_muzzleDevice002002').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+//upper receiver
+document.getElementById('buttonProductDetail_upperReceiver001001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+/* ----------- Lower Part ----------- */
+
+//bolt catch
+document.getElementById('buttonProductDetail_boltCatch00100101').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+//buffer and spring
+document.getElementById('buttonProductDetail_bufferAndSpringKit001001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+//buffer tube
+document.getElementById('buttonProductDetail_bufferTube001001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+//end plate
+document.getElementById('buttonProductDetail_endPlate001001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+document.getElementById('buttonProductDetail_endPlate002001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+//lower receiver
+document.getElementById('buttonProductDetail_lowerReceiver001001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+//magazine
+document.getElementById('buttonProductDetail_magazine001001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+document.getElementById('buttonProductDetail_magazine002001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+//magazine release
+document.getElementById('buttonProductDetail_magazineRelease001001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+document.getElementById('buttonProductDetail_magazineRelease002001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+//pistol grip
+document.getElementById('buttonProductDetail_pistolGrip001001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+document.getElementById('buttonProductDetail_pistolGrip002001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+//safety
+document.getElementById('buttonProductDetail_safety001001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+document.getElementById('buttonProductDetail_safety002001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+//stock
+document.getElementById('buttonProductDetail_stock001001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+document.getElementById('buttonProductDetail_stock002001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+//take down pin
+document.getElementById('buttonProductDetail_takedownPinSet001001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+document.getElementById('buttonProductDetail_takedownPinSet002001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+document.getElementById('buttonProductDetail_takedownPinSet003001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+//trigger
+document.getElementById('buttonProductDetail_trigger001001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+document.getElementById('buttonProductDetail_trigger002001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+//trigger guard
+document.getElementById('buttonProductDetail_triggerGuard001001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+document.getElementById('buttonProductDetail_triggerGuard002001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+/* ----------- Gear And Accecories ----------- */
+
+//bipod
+document.getElementById('buttonProductDetail_bipod001001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+//front sight
+document.getElementById('buttonProductDetail_frontSight001001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+document.getElementById('buttonProductDetail_frontSight002001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+
+//mlok and keymod rail
+document.getElementById('buttonProductDetail_mlokAndKeymodRail001001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+document.getElementById('buttonProductDetail_mlokAndKeymodRail002001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+
+//optic sight
+document.getElementById('buttonProductDetail_opticSight001001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+//rear sight
+document.getElementById('buttonProductDetail_rearSight001001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
+
+document.getElementById('buttonProductDetail_rearSight002001').addEventListener('click', function() {
+    showModal_ProductDetail();
+});
---
+++ assets/js/uiSummaryController/uiSummaryController.js
@@ -0,0 +1,18 @@
+const modalSummary = document.getElementById("modalSummary");
+
+function showModal_Summary() {
+    modalSummary.classList.add ("open");
+}
+
+function closeModal_Summary() {
+    modalSummary.classList.remove ("open");
+}
+
+document.getElementById('buttonClose_ModalSummary').addEventListener('click', function() {
+    closeModal_Summary();
+});
+
+document.getElementById('summaryChartButton').addEventListener('click', function() {
+    showModal_Summary();
+});
+
